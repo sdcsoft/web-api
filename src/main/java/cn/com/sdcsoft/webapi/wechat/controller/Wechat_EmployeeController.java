ce34ba0b7e5a4de72883918066cf7c8566c043a7
dusen <1005735462@qq.com>
2019-07-17 16:17:28 +0800

    修改设备管理接口

--- src/main/java/cn/com/sdcsoft/webapi/wechat/controller/Wechat_EmployeeController.java
+++ src/main/java/cn/com/sdcsoft/webapi/wechat/controller/Wechat_EmployeeController.java
@@ -2,12 +2,18 @@ package cn.com.sdcsoft.webapi.wechat.controller;
 
 
 import cn.com.sdcsoft.webapi.entity.Result;
+import cn.com.sdcsoft.webapi.entity.datacenter.Employee;
 import cn.com.sdcsoft.webapi.fegins.datacore.LAN_API;
 import cn.com.sdcsoft.webapi.wechat.entity.Relation_DevicePermissionMap;
 import cn.com.sdcsoft.webapi.wechat.mapper.Relation_DevicePermissionMapMapper;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import com.mysql.cj.xdevapi.JsonArray;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.LinkedHashMap;
 import java.util.List;
 
 @RestController
@@ -42,5 +48,23 @@ public class Wechat_EmployeeController {
     @GetMapping(value = "/find", produces = { "application/json;charset=UTF-8" })
     public String find(String loginId) {
         return lan_api.employeeFind(loginId);
+    }
+    //微信管理后台登录
+    @PostMapping(value = "/login", produces = { "application/json;charset=UTF-8" })
+    public Result login(String account,String passWord) {
+        JSONObject json = JSONObject.parseObject(lan_api.employeeFind(account));
+        JSONObject employee=(JSONObject)json.get("data");
+        String password=employee.get("password").toString();
+        if (null == employee)
+            return Result.getFailResult(0,"用户名或者密码输入错误");
+        if (employee.getString("password").equals(passWord)) {
+            if (Employee.STATUS_ENABLE == employee.getIntValue("status")) {
+                return Result.getFailResult(1,"success");
+            } else {
+                return Result.getFailResult(0,"您的用户账号被禁用，请联系系统管理人员！");
+            }
+        } else {
+            return Result.getFailResult(0,"用户名或者密码输入错误");
+        }
     }
 }
