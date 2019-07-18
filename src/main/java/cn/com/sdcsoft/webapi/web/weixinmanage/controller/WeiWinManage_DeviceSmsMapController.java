947f5829fb4751e38634e819daa05b00d5f2d4f9
dusen <1005735462@qq.com>
2019-07-18 16:23:35 +0800

    修改微信管理后台接口

---
+++ src/main/java/cn/com/sdcsoft/webapi/web/weixinmanage/controller/WeiWinManage_DeviceSmsMapController.java
@@ -0,0 +1,74 @@
+package cn.com.sdcsoft.webapi.web.weixinmanage.controller;
+import cn.com.sdcsoft.webapi.entity.Result;
+import cn.com.sdcsoft.webapi.web.weixinmanage.entity.Relation_DeviceSmsMap;
+import cn.com.sdcsoft.webapi.web.weixinmanage.mapper.WeiWinManage_DeviceSmsMapMapper;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.sql.Timestamp;
+
+
+@RestController
+@RequestMapping(value = "/weixinmanage/deviceSms")
+public class WeiWinManage_DeviceSmsMapController {
+    @Autowired(required=true)
+    private WeiWinManage_DeviceSmsMapMapper deviceSmsMapMapper;
+
+
+
+
+
+    /**
+     * 查询设备列表-分页
+     * @param rdc
+     * @param pageNum
+     * @param pageSize
+     * @return
+     */
+    @GetMapping(value = "/relation_deviceSmslistbyconditionandpage")
+    public Result getRelation_DeviceSmsMapMapperListByConditionAndPage(Relation_DeviceSmsMap rdc, int pageNum, int pageSize) {
+        PageHelper.startPage(pageNum, pageSize);
+        return Result.getSuccessResult(new PageInfo(deviceSmsMapMapper.getRelation_DeviceSmsMapListByCondition(rdc)));
+    }
+
+    /**
+     * 查询设备列表
+     * @param rdc
+     * @return
+     */
+    @GetMapping(value = "/relation_deviceSmslistbycondition")
+    public Result getRelation_DeviceSmsMapMapperListByCondition(Relation_DeviceSmsMap rdc) {
+        return Result.getSuccessResult(deviceSmsMapMapper.getRelation_DeviceSmsMapList(rdc));
+    }
+    /**
+     * 编辑设备
+     * @param rdc
+     * @return
+     */
+    @PostMapping("/editrelation_DeviceSmsMap")
+    public Result editRelation_DeviceSmsMap(@RequestBody Relation_DeviceSmsMap rdc){
+        if(rdc.getId()!=null){
+            deviceSmsMapMapper.updateRelation_DeviceSmsMap(rdc);
+        }else{
+            Timestamp d = new Timestamp(System.currentTimeMillis());
+            rdc.setCreateDatetime(d);
+            deviceSmsMapMapper.insertRelation_DeviceSmsMap(rdc);
+        }
+        return Result.getSuccessResult();
+    }
+
+    /**
+     * 删除设备
+     * @param id
+     * @return
+     */
+    @PostMapping(value = "/deleterelation_DeviceSmsMapbyid")
+    public Result deleteRelation_DeviceSmsMapById(@RequestParam int id){
+        deviceSmsMapMapper.deleteRelation_DeviceSmsMap(id);
+        return Result.getSuccessResult();
+    }
+
+}
+
---
+++ src/main/java/cn/com/sdcsoft/webapi/web/weixinmanage/entity/Relation_DeviceSmsMap.java
@@ -0,0 +1,51 @@
+package cn.com.sdcsoft.webapi.web.weixinmanage.entity;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import java.io.Serializable;
+import java.sql.Timestamp;
+
+public class Relation_DeviceSmsMap implements Serializable {
+
+    public static final int STATUS_ENABLE=1;
+    public static final int STATUS_DISABLE=0;
+    private Integer id;
+    private String deviceNo,employeeMobile;
+    private Timestamp createDatetime;
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getDeviceNo() {
+        return deviceNo;
+    }
+
+    public void setDeviceNo(String deviceNo) {
+        this.deviceNo = deviceNo;
+    }
+
+    public String getEmployeeMobile() {
+        return employeeMobile;
+    }
+
+    public void setEmployeeMobile(String employeeMobile) {
+        this.employeeMobile = employeeMobile;
+    }
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
+    public Timestamp getCreateDatetime() {
+        return createDatetime;
+    }
+
+    public void setCreateDatetime(Timestamp createDatetime) {
+        this.createDatetime = createDatetime;
+    }
+
+
+
+
+
+}
