601a7f94473395da9edcb1c1fe0dd6b1f84182da
dusen <1005735462@qq.com>
2019-08-05 15:47:20 +0800

    添加更新设备接口

--- src/main/java/cn/com/sdcsoft/webapi/mapper/Wechat_DB/Wechat_DB_StoreMapper.java
+++ src/main/java/cn/com/sdcsoft/webapi/mapper/Wechat_DB/Wechat_DB_StoreMapper.java
@@ -1,5 +1,4 @@
 package cn.com.sdcsoft.webapi.mapper.Wechat_DB;
-
 import cn.com.sdcsoft.webapi.wechat.entity.Store;
 import org.apache.ibatis.annotations.*;
 import org.springframework.stereotype.Component;
@@ -12,8 +11,21 @@ public interface Wechat_DB_StoreMapper {
     @Select("select ImgStyle,DeviceNo,DeviceName,DeviceType,MqttName from Store where OpenId=#{openId}")
     List<Store> getWxDeviceListByopenId(@Param("openId") String openId);
 
+    @Select("select * from Store where OpenId=#{openId} and DeviceNo=#{deviceNo}")
+    Store getWxDeviceByopenIdAndDeviceNo(@Param("openId") String openId,@Param("deviceNo") String deviceNo);
+
     @Insert("insert into Store(ImgStyle,DeviceNo,OpenId,DeviceName,DeviceType,MqttName) values (#{imgStyle},#{deviceNo},#{openId},#{deviceName},#{deviceType},#{mqttName})")
     int insertWxDevice(Store store);
+
+    @Insert("<script>"+
+            "insert into Store(DeviceNo,ImgStyle,OpenId,DeviceName,DeviceType,MqttName)"
+            + "values "
+            + "<foreach collection =\"storeList\" item=\"store\" index=\"index\" separator =\",\"> "
+            + "(#{store.deviceNo},#{store.imgStyle},#{store.openId},#{store.deviceName},#{store.deviceType},#{store.mqttName})"
+            + "</foreach > " +
+            "</script>")
+    int insertManyStore(@Param("storeList") List<Store> storeList);
+
 
     @Delete("delete from Store where DeviceNo=#{deviceNo}and OpenId=#{openId}")
     int deleteWxDevice(@Param("deviceNo")String deviceNo,@Param("openId")String openId);
--- src/main/java/cn/com/sdcsoft/webapi/wechat/controller/Wechat_StoreController.java
+++ src/main/java/cn/com/sdcsoft/webapi/wechat/controller/Wechat_StoreController.java
@@ -4,8 +4,12 @@ import cn.com.sdcsoft.webapi.entity.Result;
 import cn.com.sdcsoft.webapi.fegins.datacore.LAN_API;
 import cn.com.sdcsoft.webapi.wechat.entity.Store;
 import cn.com.sdcsoft.webapi.mapper.Wechat_DB.Wechat_DB_StoreMapper;
+import com.alibaba.fastjson.JSON;
+import com.mysql.cj.xdevapi.JsonArray;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 
 @RestController
@@ -43,5 +47,19 @@ public class Wechat_StoreController {
         return Result.getSuccessResult();
     }
 
+    @PostMapping("/create/many")
+    public Result insertManyStore(String storeList){
+        List<Store> list=JSON.parseArray(storeList,Store.class);
+        for(int i=0;i<list.size();i++){
+           if(wechatDBDeviceMapper.getWxDeviceByopenIdAndDeviceNo(list.get(i).getOpenId(),list.get(i).getDeviceNo())!=null){
+               list.remove(i);
+           }
+        }
+        if(list.size()>0){
+            wechatDBDeviceMapper.insertManyStore(list);
+            return Result.getSuccessResult();
+        }
+        return Result.getFailResult("插入失败");
+    }
 }
 
