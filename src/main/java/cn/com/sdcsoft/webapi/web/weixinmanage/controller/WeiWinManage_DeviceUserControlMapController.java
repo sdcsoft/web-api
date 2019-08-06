2082d697bfd319dad86b1feacf411261dd1c4038
dusen <1005735462@qq.com>
2019-08-06 15:33:01 +0800

    修改设备控制接口

--- src/main/java/cn/com/sdcsoft/webapi/web/weixinmanage/controller/WeiWinManage_DeviceUserControlMapController.java
+++ src/main/java/cn/com/sdcsoft/webapi/web/weixinmanage/controller/WeiWinManage_DeviceUserControlMapController.java
@@ -4,6 +4,7 @@ import cn.com.sdcsoft.webapi.entity.Result;
 import cn.com.sdcsoft.webapi.fegins.datacore.LAN_API;
 import cn.com.sdcsoft.webapi.mapper.Wechat_DB.Wechat_DB_DeviceUserControlMapMapper;
 import cn.com.sdcsoft.webapi.web.weixinmanage.entity.DeviceUserControlMap;
+import com.alibaba.fastjson.JSONObject;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -36,8 +37,11 @@ public class WeiWinManage_DeviceUserControlMapController {
         if(rdc.getId()!=null){
             rdcMapper.updateDeviceUserControlMap(rdc);
         }else{
+            JSONObject json = JSONObject.parseObject(lan_api.employeeFind(rdc.getEmployeeMobile()));
+            JSONObject employee=(JSONObject)json.get("data");
             Timestamp d = new Timestamp(System.currentTimeMillis());
             rdc.setCreateDatetime(d);
+            rdc.setOpenId(employee.getString("weiXin"));
             rdcMapper.insertDeviceUserControlMap(rdc);
         }
         return Result.getSuccessResult();
