a0623a1e0b1a80eccc55ab3affc477295a8583cd
dusen <1005735462@qq.com>
2020-01-10 16:37:54 +0800

    添加检查展会版openid

--- src/main/java/cn/com/sdcsoft/webapi/mapper/Wechat_DB/Wechat_DB_WechatUserMapper.java
+++ src/main/java/cn/com/sdcsoft/webapi/mapper/Wechat_DB/Wechat_DB_WechatUserMapper.java
@@ -3,6 +3,8 @@ import cn.com.sdcsoft.webapi.wechat.entity.WechatUser;
 import org.apache.ibatis.annotations.*;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 
 @Component
 public interface Wechat_DB_WechatUserMapper {
@@ -10,5 +12,6 @@ public interface Wechat_DB_WechatUserMapper {
     @Insert("insert into WechatUser(OpenId,RealName,CreateDatetime) values (#{openId},#{realName},#{createDatetime})")
     int insertWechatUser(WechatUser wechatUser);
 
-
+    @Select("select * from WechatUser where OpenId=#{openId}")
+    List<WechatUser> getWechatUserListByopenId(@Param("openId") String openId);
 }
