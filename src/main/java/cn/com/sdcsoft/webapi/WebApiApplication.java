3feff9a830a068d55d105471fc0bbeeac639cf26
jialiang.li <80201288@qq.com>
2020-11-11 23:06:02 +0800

    修改mongodb自动连接127.0.0.1，暂时无效果

--- src/main/java/cn/com/sdcsoft/webapi/WebApiApplication.java
+++ src/main/java/cn/com/sdcsoft/webapi/WebApiApplication.java
@@ -1,19 +1,24 @@
 package cn.com.sdcsoft.webapi;
 
+import cn.com.sdcsoft.webapi.config.db_config.DeviceSettingMongoConfig;
+import cn.com.sdcsoft.webapi.config.db_config.PrimaryMongoConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
-
-@SpringBootApplication
 @EnableEurekaClient
 @EnableFeignClients
 @EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
 @EnableCaching
+@SpringBootApplication(exclude={MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
+@EnableConfigurationProperties({PrimaryMongoConfig.class,DeviceSettingMongoConfig.class})
 public class WebApiApplication {
 
     public static void main(String[] args) {
